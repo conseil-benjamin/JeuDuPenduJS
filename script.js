29ba0e7ef81b0ec8421d77da822603a1ea5f58ff
conseil-benjamin <benlenormand61@gmail.com>
2023-07-21 22:45:31 +0200

    Problème affichage lettre mot révélé réglé

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-/*
-function miseEnPlace(motRandom) {
-  let lettre = document.querySelector("#lettre");
-  
-
-  // vérifier si la lettre est correcte si il n'y en a pas plusieurs
-  /* if(lettre != ) {
-
-  }
-
-  console.log(motRandom);
-
-  // mise en place mot avec des underscore
-  for (let i = 0; i < motRandom.length; i++) {
-    result = motRandom.replace(i, "_");
-  }
-  
-} 
-
-*/
-
 // commencement du jeu à l'appuie du bouton jouer et initialisation d'un mot pour le jeu
+let motGenere = false;
+
+let motRandom;
+let indexMotRandom;
+let motRandomRevele;
+
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substring(0, index) +
+    replacement +
+    this.substring(index + replacement.length)
+  );
+};
+
 function lancement() {
   let zoneJeu = document.querySelector(".zoneJeu");
   zoneJeu.style.display = "block";
@@ -27,23 +20,35 @@ function lancement() {
   let btnJouer = document.querySelector("#btn-Jouer");
   btnJouer.style.display = "none";
 
+  let lettre = document.querySelector("#lettre");
+
   let mot = document.querySelector("#textePendu");
 
-  // on affiche un mot aléatoire
-  let motRandom = motsCaches[Math.floor(Math.random() * motsCaches.length)];
-  let indexMotRandom = motsCaches.indexOf(motRandom);
-  console.log(indexMotRandom); // marche bien
-  let motRandomRevele = mots[indexMotRandom];
+  let btnRejouer = document.querySelector("#btn-rejouer");
 
-  mot.textContent = motRandom;
+  if (!motGenere) {
+    // on affiche un mot aléatoire
+    motRandom = motsCaches[Math.floor(Math.random() * motsCaches.length)];
+    indexMotRandom = motsCaches.indexOf(motRandom);
+    motRandomRevele = mots[indexMotRandom];
+    mot.textContent = motRandom;
+  }
 
+  motGenere = true;
+
+  btnRejouer.addEventListener("click", () => {
+    lettre.textContent = " ";
+    motGenere = false;
+    lancement();
+  });
   jeu(motRandom, motRandomRevele);
 }
 
 function jeu(motRandom, motRandomRevele) {
   let lettre = document.querySelector("#lettre");
-
+  let gagne = false;
   let nbChances = 10;
+  let lettreBonne = false;
   let nbChancesElement = document.querySelector("#nbChances");
 
   let mot = document.querySelector("#textePendu");
@@ -55,27 +60,49 @@ function jeu(motRandom, motRandomRevele) {
 
   let lettreTape = lettre.value;
   console.log(lettreTape);
+  console.log(motRandomRevele);
 
   for (let i = 0; i < motRandomRevele.length; i++) {
     if (lettreTape === motRandomRevele[i]) {
-      let result = motRandom.replace(motRandom[4], motRandomRevele[1]); // marche dans la fonction lancement, la régler pour cette fonction
-      mot.textContent(result); // permet d'actualiser l'affichage du mot
+      motRandom = motRandom.replaceAt(i * 2, motRandomRevele[i]); // marche également avec lettreTape
+      lettreBonne = true;
     } else {
-      nbChances--; // a changer de place car probleme
+      lettreBonne = false;
     }
   }
 
-  // régler problème doublon ou plus pour une lettre si cela n'est pas régler de base
+  console.log(motRandom);
 
+  mot.textContent = motRandom;
+  console.log(motRandom);
+
+  // vérification si le mot écrit par l'user === motRandomRevele
   // on vérifie si le joueur à encore une chance ou non
-
-  if (nbChances === 0) {
-    finJeu(motRandom);
+  if (motRandom === motRandomRevele) {
+    gagne = true;
+    finJeu(gagne, motRandomRevele);
+  } else if (nbChances === 0) {
+    gagne = false;
+    finJeu(gagne, motRandomRevele);
   } else {
-    nbChancesElement.textContent = "Nombres de chances :" + " " + nbChances;
+    if (!lettreBonne) {
+      nbChances--;
+      let message = "Nombres de chances : " + nbChances + "<br>Mauvaise lettre";
+      document.getElementById("nbChances").innerHTML = message;
+    } else {
+      nbChancesElement.textContent = "Nombres de chance : " + nbChances;
+    }
   }
+
+  console.log(motRandom);
+
+  // régler problème doublon ou plus pour une lettre si cela n'est pas régler de base
 }
 
-function finJeu(mot) {
-  alert("Perdu ! Le mot était : " + mot);
+function finJeu(gagne, mot) {
+  if (gagne) {
+    alert("Félicitations vous avez trouvez le mot :" + mot);
+  } else {
+    alert("Perdu ! Le mot était : " + mot);
+  }
 }
